910295a5b4744842020517832e6e116d4cab876f
Brage Kristoffersen <bragekris@hotmail.com>
2024-11-11 13:10:56 +0100

    Første commit

--- barnehage/kg.py
+++ barnehage/kg.py
@@ -1,14 +1,9 @@
-from flask import Flask
-from flask import url_for
-from flask import render_template
-from flask import request
-from flask import redirect
-from flask import session
-from kgmodel import (Foresatt, Barn, Soknad, Barnehage)
-from kgcontroller import (form_to_object_soknad, insert_soknad, commit_all, select_alle_barnehager)
+from flask import Flask, url_for, render_template, request, redirect, session
+from kgcontroller import (form_to_object_soknad, insert_soknad, commit_all, select_alle_barnehager, select_alle_soknader)
 
+# Definerer Flask-applikasjonen med riktig '_name_' variabel
 app = Flask(__name__)
-app.secret_key = 'BAD_SECRET_KEY' # nødvendig for session
+app.secret_key = 'BAD_SECRET_KEY'  # nødvendig for session
 
 @app.route('/')
 def index():
@@ -16,40 +11,84 @@ def index():
 
 @app.route('/barnehager')
 def barnehager():
+    # Henter informasjon om alle barnehager
     information = select_alle_barnehager()
     return render_template('barnehager.html', data=information)
 
 @app.route('/behandle', methods=['GET', 'POST'])
 def behandle():
     if request.method == 'POST':
+        # Henter data fra skjemaet
         sd = request.form
         print(sd)
-        log = insert_soknad(form_to_object_soknad(sd))
-        print(log)
+        
+        # Lager en søknad basert på skjemaet
+        soknad_objekt = form_to_object_soknad(sd)
+        
+        # Setter inn søknaden i "databasen"
+        insert_soknad(soknad_objekt)
+        
+        # Lagre skjemaopplysningene i session for senere bruk
         session['information'] = sd
-        return redirect(url_for('svar')) #[1]
+
+        # Vurderer status basert på antall ledige plasser og fortrinnsrett
+        ledige_plasser = 5  # Dette bør hentes dynamisk fra databasen
+        status = "AVSLAG"
+
+        if ledige_plasser > 0:
+            status = "TILBUD"
+        elif (sd.get('fortrinnsrett_barnevern') or 
+              sd.get('fortrinnsrett_sykdom_i_familien') or 
+              sd.get('fortrinnsrett_sykdome_paa_barnet')):
+            status = "Fortrinnsrett, venter på ledig plass"
+
+        # Går videre til svar-siden og gir status
+        return render_template('svar.html', data=sd, status=status)
     else:
         return render_template('soknad.html')
 
 @app.route('/svar')
 def svar():
-    information = session['information']
-    return render_template('svar.html', data=information)
+    if 'information' in session:
+        # Henter informasjonen fra session
+        information = session['information']
+        
+        # Viser informasjonen og gir standard status (dette vil vanligvis allerede være gjort i /behandle)
+        status = "AVSLAG"  # Standardverdi som kan endres basert på ledige plasser
+        return render_template('svar.html', data=information, status=status)
+    else:
+        # Hvis det ikke er noen informasjon i session, går tilbake til hovedsiden
+        return redirect(url_for('index'))
 
 @app.route('/commit')
 def commit():
+    # Skriver alle endringer til Excel-filen
     commit_all()
-    return render_template('commit.html')
+    
+    # Henter alle søknader for å vise dem etter at de er lagret
+    alle_soknader = select_alle_soknader()
+    
+    return render_template('commit.html', soknader=alle_soknader)
 
+@app.route('/soeknader')
+def soeknader():
+    # Henter alle søknader fra "databasen"
+    alle_soknader = select_alle_soknader()
+    ledige_plasser = 5  # Dette bør justeres dynamisk for å reflektere faktisk antall ledige plasser fra databasen
+    
+    # Beregner status for hver søknad basert på ledige plasser og fortrinnsrett
+    for soknad in alle_soknader:
+        if ledige_plasser > 0:
+            soknad.status = "TILBUD"
+            ledige_plasser -= 1
+        elif soknad.fr_barnevern or soknad.fr_sykd_familie or soknad.fr_sykd_barn:
+            soknad.status = "Fortrinnsrett, venter på ledig plass"
+        else:
+            soknad.status = "AVSLAG"
 
+    # Viser alle søknader i soeknader.html
+    return render_template('soeknader.html', soknader=alle_soknader)
 
-
-"""
-Referanser
-[1] https://stackoverflow.com/questions/21668481/difference-between-render-template-and-redirect
-"""
-
-"""
-Søkeuttrykk
-
-"""
+# Start Flask-serveren hvis dette skriptet kjøres direkte
+if __name__ == "__main__":
+    app.run(debug=False)
--- barnehage/kgcontroller.py
+++ barnehage/kgcontroller.py
@@ -8,18 +8,8 @@ from kgmodel import *
 # CRUD metoder
 
 # Create
-# pd.append, pd.concat eller df.loc[-1] = [1,2] df.index = df.index + 1 df = df.sort_index()
+
 def insert_foresatt(f):
-    # Ikke en god praksis å oppdaterer DataFrame ved enhver endring!
-    # DataFrame er ikke egnet som en databasesystem for webapplikasjoner.
-    # Vanligvis bruker man databaseapplikasjoner som MySql, Postgresql, sqlite3 e.l.
-    # 3 fremgangsmåter for å oppdatere DataFrame:
-    # (1) df.colums er [['a', 'b']]
-    #     df = pd.concat([pd.DataFrame([[1,2]], columns=df.columns), df], ignore_index=True)
-    # (2) df = df.append({'a': 1, 'b': 2}, ignore_index=True)
-    # (3) df.loc[-1] = [1,2]
-    #     df.index = df.index + 1
-    #     df = df.sort_index()
     global forelder
     new_id = 0
     if forelder.empty:
@@ -27,15 +17,16 @@ def insert_foresatt(f):
     else:
         new_id = forelder['foresatt_id'].max() + 1
     
-    # skriv kode for å unngå duplikater
-    
+    # sjekk for duplikater, om mulig
+    if not forelder[forelder['foresatt_pnr'] == f.foresatt_pnr].empty:
+        return forelder  # Returner hvis foresatt allerede finnes
+
     forelder = pd.concat([pd.DataFrame([[new_id,
                                         f.foresatt_navn,
                                         f.foresatt_adresse,
                                         f.foresatt_tlfnr,
                                         f.foresatt_pnr]],
                 columns=forelder.columns), forelder], ignore_index=True)
-    
     
     return forelder
 
@@ -47,29 +38,26 @@ def insert_barn(b):
     else:
         new_id = barn['barn_id'].max() + 1
     
-    # burde også sjekke for samme foresatt_pnr for å unngå duplikater
-    
-    barn = pd.concat([pd.DataFrame([[new_id,
-                                    b.barn_pnr]],
-                columns=barn.columns), barn], ignore_index=True)
+    # sjekk for duplikater
+    if not barn[barn['barn_pnr'] == b.barn_pnr].empty:
+        return barn  # Returner hvis barn allerede finnes
+
+    barn = pd.concat([pd.DataFrame([[new_id, b.barn_pnr]], columns=barn.columns), barn], ignore_index=True)
     
     return barn
 
 def insert_soknad(s):
-    """[sok_id, foresatt_1, foresatt_2, barn_1, fr_barnevern, fr_sykd_familie,
-    fr_sykd_barn, fr_annet, barnehager_prioritert, sosken__i_barnehagen,
-    tidspunkt_oppstart, brutto_inntekt]
-    """
     global soknad
     new_id = 0
     if soknad.empty:
         new_id = 1
     else:
         new_id = soknad['sok_id'].max() + 1
-    
-    
-    # burde også sjekke for duplikater
-    
+
+    # sjekk for duplikater
+    if not soknad[(soknad['foresatt_1'] == s.foresatt_1.foresatt_id) & (soknad['barn_1'] == s.barn_1.barn_id)].empty:
+        return soknad  # Returner hvis søknad allerede finnes
+
     soknad = pd.concat([pd.DataFrame([[new_id,
                                      s.foresatt_1.foresatt_id,
                                      s.foresatt_2.foresatt_id,
@@ -112,10 +100,39 @@ def select_barn(b_pnr):
         return np.nan
     else:
         return series.iloc[0] # returnerer kun det første elementet i series
-    
-    
-# --- Skriv kode for select_soknad her
 
+def select_alle_soknader():
+    """Returnerer en liste med alle søknader definert i databasen dbexcel."""
+    return soknad.apply(lambda r: Soknad(
+                             r['sok_id'],
+                             select_foresatt_by_id(r['foresatt_1']),
+                             select_foresatt_by_id(r['foresatt_2']),
+                             select_barn_by_id(r['barn_1']),
+                             r['fr_barnevern'],
+                             r['fr_sykd_familie'],
+                             r['fr_sykd_barn'],
+                             r['fr_annet'],
+                             r['barnehager_prioritert'],
+                             r['sosken__i_barnehagen'],
+                             r['tidspunkt_oppstart'],
+                             r['brutto_inntekt']),
+         axis=1).to_list()
+
+def select_foresatt_by_id(foresatt_id):
+    """Henter foresatt basert på ID"""
+    row = forelder[forelder['foresatt_id'] == foresatt_id]
+    if not row.empty:
+        r = row.iloc[0]
+        return Foresatt(r['foresatt_id'], r['foresatt_navn'], r['foresatt_adresse'], r['foresatt_tlfnr'], r['foresatt_pnr'])
+    return None
+
+def select_barn_by_id(barn_id):
+    """Henter barn basert på ID"""
+    row = barn[barn['barn_id'] == barn_id]
+    if not row.empty:
+        r = row.iloc[0]
+        return Barn(r['barn_id'], r['barn_pnr'])
+    return None
 
 # ------------------
 # Update
@@ -136,27 +153,7 @@ def commit_all():
         
 # --- Diverse hjelpefunksjoner ---
 def form_to_object_soknad(sd):
-    """sd - formdata for soknad, type: ImmutableMultiDict fra werkzeug.datastructures
-Eksempel:
-ImmutableMultiDict([('navn_forelder_1', 'asdf'),
-('navn_forelder_2', ''),
-('adresse_forelder_1', 'adf'),
-('adresse_forelder_2', 'adf'),
-('tlf_nr_forelder_1', 'asdfsaf'),
-('tlf_nr_forelder_2', ''),
-('personnummer_forelder_1', ''),
-('personnummer_forelder_2', ''),
-('personnummer_barnet_1', '234341334'),
-('personnummer_barnet_2', ''),
-('fortrinnsrett_barnevern', 'on'),
-('fortrinnsrett_sykdom_i_familien', 'on'),
-('fortrinnsrett_sykdome_paa_barnet', 'on'),
-('fortrinssrett_annet', ''),
-('liste_over_barnehager_prioritert_5', ''),
-('tidspunkt_for_oppstart', ''),
-('brutto_inntekt_husholdning', '')])
-    """
-    # Lagring i hurtigminne av informasjon om foreldrene (OBS! takler ikke flere foresatte)
+    """sd - formdata for soknad, type: ImmutableMultiDict fra werkzeug.datastructures"""
     foresatt_1 = Foresatt(0,
                           sd.get('navn_forelder_1'),
                           sd.get('adresse_forelder_1'),
@@ -170,17 +167,13 @@ ImmutableMultiDict([('navn_forelder_1', 'asdf'),
                           sd.get('personnummer_forelder_2'))
     insert_foresatt(foresatt_2) 
     
-    # Dette er ikke elegang; kunne returnert den nye id-en fra insert_ metodene?
     foresatt_1.foresatt_id = select_foresatt(sd.get('navn_forelder_1'))
     foresatt_2.foresatt_id = select_foresatt(sd.get('navn_forelder_2'))
     
-    # Lagring i hurtigminne av informasjon om barn (OBS! kun ett barn blir lagret)
     barn_1 = Barn(0, sd.get('personnummer_barnet_1'))
     insert_barn(barn_1)
     barn_1.barn_id = select_barn(sd.get('personnummer_barnet_1'))
     
-    # Lagring i hurtigminne av all informasjon for en søknad (OBS! ingen feilsjekk / alternativer)
-        
     sok_1 = Soknad(0,
                    foresatt_1,
                    foresatt_2,
@@ -195,11 +188,3 @@ ImmutableMultiDict([('navn_forelder_1', 'asdf'),
                    sd.get('brutto_inntekt_husholdning'))
     
     return sok_1
-
-# Testing
-def test_df_to_object_list():
-    assert barnehage.apply(lambda r: Barnehage(r['barnehage_id'],
-                             r['barnehage_navn'],
-                             r['barnehage_antall_plasser'],
-                             r['barnehage_ledige_plasser']),
-         axis=1).to_list()[0].barnehage_navn == "Sunshine Preschool"
